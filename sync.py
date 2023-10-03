7c2021cb89a373e2ddb83e2c2d4361fe27c7d3d5
Jakob Lechner <mail@jalr.de>
2023-10-03 13:47:10 +0000

    Implement file upload

--- sync.py
+++ sync.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-import urllib.request
-import os
-from itertools import chain
 from urllib.parse import urlencode
+import binascii
 import json
+import os
+import sys
+import urllib.request
 
 base_url = "http://laserkutter.lab.fablab-nea.de"
 file_path = os.path.dirname(__file__)
@@ -17,6 +18,11 @@ def to_local_path(remote_path):
         remote_path = remote_path[1:]
     return os.path.join(sd_card_directory,
                         *remote_path.split(RRF_PATH_SEPARATOR))
+
+
+def to_remote_path(local_path):
+    RRF_PATH_SEPARATOR = '/'
+    return RRF_PATH_SEPARATOR.join(local_path.split(os.sep))
 
 
 def list_remote_files(drive, directory):
@@ -63,4 +69,55 @@ def download_files_recursively(drive="0", directory=""):
             download_file(drive, path)
 
 
-download_files_recursively()
+def upload_files_recursively(drive="0", directory=""):
+    session_key = authenticate()
+    for directory, _, files in os.walk(sd_card_directory):
+        for file in files:
+            rel_path = os.path.join(
+                os.path.relpath(directory, sd_card_directory),
+                file
+            )
+            print(rel_path)
+            with open(
+                os.path.join(directory, file),
+                'rb'
+            ) as f:
+                data = f.read()
+
+            crc = hex(binascii.crc32(data)).split('x')[-1]
+
+            query_string = urlencode({
+                'name': f"{drive}:/{to_remote_path(rel_path)}",
+                'crc32': crc,
+            })
+
+            urllib.request.urlopen(
+                urllib.request.Request(
+                    f"{base_url}/rr_upload?" + query_string,
+                    data=data,
+                    headers={
+                        'Content-Length': len(data),
+                        'X-Session-Key': session_key
+                    }
+                )
+            )
+
+
+def authenticate():
+    return json.load(
+        urllib.request.urlopen(
+            f"{base_url}/rr_connect?password=&sessionKey=yes"
+        )
+    )['sessionKey']
+
+
+def main():
+    command = sys.argv[1]
+    if command == "download":
+        download_files_recursively()
+    elif command == "upload":
+        upload_files_recursively()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
